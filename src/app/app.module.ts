7c0bbd2895ad6a07c340fdcd77358fcdd03f1927
SarthakKathuria <sarthakkathuria2001@gmail.com>
2021-06-01 16:15:00 +0530

    creating multiple modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AppComponent } from './app.component';
 import { HelloWorldComponent } from './hello-world/hello-world.component';
 import { DateComponent } from './date/date.component';
 import { FormsModule } from '@angular/forms';
+//this is typescript import 
+import { ViewModule } from './view/view.module';
+
 
 @NgModule({
   declarations: [
@@ -15,7 +18,10 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     // for two way data binding
-    FormsModule
+    FormsModule,
+    //using multiple modules
+    //this is angular import to use different class from different view or comonent
+    ViewModule
   ],
   providers: [],
   bootstrap: [AppComponent]
